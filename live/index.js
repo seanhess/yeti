50d58ef90784a3be4e9b48f4fe9c1fda61fe476f
Sean Hess <seanhess@gmail.com>
2022-09-22 09:58:55 -0600

    Handling click events!

--- live/index.js
+++ live/index.js
@@ -14,22 +14,19 @@ var rootElement
 
 // Connection opened
 socket.addEventListener('open', (event) => {
-    // we are connecting to our path
-    // we need to establish a protocol
     console.log("Open")
+
+    // 1. send our initial state to register
     socket.send(juniperState);
 });
 
 // Listen for messages
 socket.addEventListener('message', (event) => {
-    console.log('Message from server ', event.data);
-
     let dom = fromStringToDomNode(event.data)
     let vnode = fromDomNodeToVNode(dom)
 
     // This works, but it REALLY doesn't like the unclosed input tags from lucid
     rootElement = patch(rootElement, vnode)
-    console.log(rootElement)
 });
 
 socket.addEventListener('close', (e) => {
@@ -44,13 +41,16 @@ window.addEventListener("load", function() {
   console.log("State:", juniperState)
   rootElement = document.getElementById("juniper-root-content")
 
-  // Hydrate it! (hydrate doesn't work, ironically)
+  // 0. Hydrate content so we don't lose focus, etc
   let initContent = fromDomNodeToVNode(rootElement.children[0])
   rootElement = patch(rootElement, initContent)
 })
 
+
+// Handle Click Events, Easy!
 document.addEventListener("click", function(e) {
-  if (e.target.type == "submit") {
-    socket.send("Increment")
+  let click = e.target.dataset.onClick
+  if (click) {
+    socket.send(click)
   }
 })
